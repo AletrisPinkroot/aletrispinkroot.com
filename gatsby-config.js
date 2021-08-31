a128a8118385ddb81ea3069ce58b7f372b1cc0b5
Aletris Pinkroot <72479926+AletrisPinkroot@users.noreply.github.com>
2021-08-31 16:22:03 +1000

    initial gatsby

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,29 @@
 module.exports = {
-  siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
-    title: "aletrispinkroot.com",
-  },
-  plugins: ["gatsby-plugin-styled-components"],
+	siteMetadata: {
+		siteUrl: 'https://www.aletrispinkroot.com',
+		title: 'aletrispinkroot.com',
+	},
+	plugins: [
+		{
+			resolve: 'gatsby-plugin-google-gtag',
+			options: {
+				trackingIds: ['G-N8FM65T6S3'],
+				gtagConfig: {
+					optimize_id: 'OPT_CONTAINER_ID',
+					anonymize_ip: true,
+					cookie_expires: 0,
+				},
+				// This object is used for configuration specific to this plugin
+				pluginConfig: {
+					// Puts tracking script in the head instead of the body
+					head: true,
+					// Setting this parameter is also optional
+					respectDNT: true,
+					// Avoids sending pageview hits from custom paths
+					exclude: ['/preview/**', '/do-not-track/me/too/'],
+				},
+			},
+		},
+		'gatsby-plugin-styled-components',
+	],
 };
